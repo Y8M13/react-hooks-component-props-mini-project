49b0b214bce9ee5bf271ccfe6c399b5a1c4cf198
Yolanda Morris <y.mijoe7145@gmail.com>
2022-03-22 10:13:22 -0400

    Updated and Deleted Components

--- src/index.js
+++ src/index.js
@@ -1,11 +1,11 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import "./index.css"
-import Header  from "../components/Header.js"
-import About from "../components/About.js"
-import ArticleList from "../components/ArticleList.js"
-import Article from "../components/Article.js"
-import App from "../components/App.js"
+// import "./index.css"
+// import Header  from "../components/Header.js"
+// import About from "../components/About.js"
+// import ArticleList from "../components/ArticleList.js"
+// import Article from "../components/Article.js"
+import App from "../components/App.js";
 
 
 
